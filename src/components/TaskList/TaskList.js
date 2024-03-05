b33cbfaabf77f923d9b04f662889dad4e31d5a89
AntonioThone <antoniothone6@gmail.com>
2024-03-05 03:53:01 +0100

    hooks...

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,11 +2,23 @@ import React from "react";
 import "./TaskList.css";
 import PropTypes from "prop-types";
 
+
+
 export default function TaskList({ title }) {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((currentCount)  => {
+      return currentCount + 1;
+    })
+  }
+
   return (
     <div className="tasklist">
       <div className="title">{title}</div>
       <div className="content"></div>
+        {count}
+        <button onClick={increment}>incrementar</button>
     </div>
   );
 }
