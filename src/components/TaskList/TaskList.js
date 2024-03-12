f436f7a8bae6d11ef1a15b1b8a18399b79eb1a39
AntonioThone <antoniothone6@gmail.com>
2024-03-12 02:51:46 +0100

    ...

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,18 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./TaskList.css";
 import PropTypes from "prop-types";
 
-
-
 export default function TaskList({ title }) {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount((currentCount)  => {
-      return currentCount + 1;
-    })
-  }
-
   return (
     <div className="tasklist">
       <div className="title">{title}</div>
